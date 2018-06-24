f99e52bc748ccc08578d41af39511432bdbd7bd4
keyphlip <philipskenan@gmail.com>
2018-06-23 21:30:55 -0700

    font change

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
-$(document).ready(function() {
+function fullPage() {
 	$('#fullpage').fullpage();
+}
+
+function activeClass() {
+	// Get the container element
+	var btnContainer = document.getElementById("fp-nav");
+
+	// Get all buttons with class="btn" inside the container
+	var btns = btnContainer.getElementsByClassName("nav-btn");
+
+	// Loop through the buttons and add the active class to the current/clicked button
+	for (var i = 0; i < btns.length; i++) {
+	  btns[i].addEventListener("click", function() {
+	    var current = document.getElementsByClassName("active");
+	    current[0].className = current[0].className.replace(" active", "");
+	    this.className += " active";
+	  });
+	}
+}
+
+$(document).ready(function() {
+	fullPage();
+	activeClass();
 });
